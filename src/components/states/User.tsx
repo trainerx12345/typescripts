5d26c3966584007e3e26df128ed165913819b89d
Sam <sambusinesstransac@gmail.com>
2023-02-01 20:58:25 +0800

    feat: type assertion

--- src/components/states/User.tsx
+++ src/components/states/User.tsx
@@ -8,6 +8,12 @@ type AuthUser = {
 export const User = () => {
   //the angle brackets give us an option whether a null or an object to be used in useState
   const [user, setUser] = useState<AuthUser | null>(null)
+  //type assertion sample for states
+  /*   
+  this makes the type assertion to bypass if logout is not possible to the environment to empty 
+  ! const [user, setUser] = useState<AuthUser>({} as AuthUsers)
+  this consider be using if there is no value in initial load
+  */
   const handleLogin = () => {
     setUser({
       name: 'Louise Bane',
